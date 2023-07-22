0f00fedb7145db6b3056efdd783f8e494f1e5059
Your Name <anggriawan@sinbad.co.id>
2023-07-22 14:10:11 +0700

    dashboard fix issue

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,5 +1,7 @@
 <?php
-include_once 'menu.php';
+include_once __DIR__ . '/../function/base.php'; // first to call have use __DIR__
+
+include_once BASE_DIR_BLOG_RATIH . '/view/menu.php';
 ?>
 
 <?php
