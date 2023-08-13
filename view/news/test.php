f11bfb069b3a750ccf85347061a901525df416f9
Affan Armansyah <affanarmansyah17@.com>
2023-08-14 00:57:14 +0700

    test php

--- view/news/test.php
+++ view/news/test.php
@@ -1,16 +1,34 @@
-1. buat array yang berisi angka 1 - 10 dengan metode for
+<!-- 1. buat array yang berisi angka 1 - 10 dengan metode for
 <br>
 <?php
 $angka = [];
 
-// Loop untuk mengisi array dengan angka 1-10
 for ($i = 1; $i <= 10; $i++) {
     $angka[] = $i;
 }
 print_r($angka);
 
-// Cetak isi array
-?>
+?> -->
 
 2. buat array yang berisi angka 1 - 10 dengan metode for dan buat juga function yang akan mengganti angka genap menjadi kata "genap"
-   contoh: [1, genap, 3, genap, 5, genap]
+contoh: [1, genap, 3, genap, 5, genap]
+<br>
+
+<?php
+
+function Genap($angkagenap)
+{
+    if ($angkagenap % 2 == 0) {
+        return "genap";
+    }
+    return true;
+}
+
+
+$angka = [];
+
+for ($i = 0; $i <= 10; $i++) {
+    $angka[] = Genap($i);
+}
+print_r($angka);
+?>
