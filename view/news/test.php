c6ec12e74df650b9160b52e11ce681984c7f119d
Your Name <anggriawan@sinbad.co.id>
2023-08-13 23:35:00 +0700

    tugas

--- view/news/test.php
+++ view/news/test.php
@@ -11,3 +11,6 @@ print_r($angka);
 
 // Cetak isi array
 ?>
+
+2. buat array yang berisi angka 1 - 10 dengan metode for dan buat juga function yang akan mengganti angka genap menjadi kata "genap"
+   contoh: [1, genap, 3, genap, 5, genap]
