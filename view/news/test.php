bf48a7f72aead41dded571e966fe4b98ed5136cc
Affan Armansyah <affanarmansyah17@.com>
2023-08-13 23:32:43 +0700

    test php

--- view/news/test.php
+++ view/news/test.php
@@ -1,1 +1,13 @@
 1. buat array yang berisi angka 1 - 10 dengan metode for
+<br>
+<?php
+$angka = [];
+
+// Loop untuk mengisi array dengan angka 1-10
+for ($i = 1; $i <= 10; $i++) {
+    $angka[] = $i;
+}
+print_r($angka);
+
+// Cetak isi array
+?>
